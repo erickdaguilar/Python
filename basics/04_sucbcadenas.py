490777192305610856343e47fce1e9fe2e37a6af
erickdaguilar <erickdaguilar0305@gmail.com>
2019-10-14 15:05:29 -0500

    Clase 14 de octubre

--- basics/04_sucbcadenas.py
+++ basics/04_sucbcadenas.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Subcadenas
+
+# In[1]:
+
+
 # -*- coding: utf-8 -*-
 
 # ------------- Cadenas y subcadenas -------------
@@ -5,12 +13,24 @@
 # Cadena con 22 caracteres
 cadena = "parangaricutirimicuaro"
 
+
+# In[2]:
+
+
 print (cadena[0])    # Se obtiene el primer carácter
 print (cadena[21])   # Se obtiene el último carácter
+
+
+# In[3]:
+
 
 # Si se pone un indice negativo, la cuenta
 # empieza en el �ltimo elemento de la cadena
 print (cadena[-2])   # último carácter
+
+
+# In[4]:
+
 
 # Si se quiere acceder a un elemento fuera de rango
 # de la cadena, se obtiene el error
@@ -21,3 +41,10 @@ print (cadena[5:13])  # imprime garicuti
 print (cadena[5:])    # imprime garicutirimicuaro
 print (cadena[:5])    # imprime param
 print (cadena[:])     # imprime la cadena completa
+
+
+# In[ ]:
+
+
+
+
--- basics/05_busquedas.py
+++ basics/05_busquedas.py
@@ -1,27 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 # -*- coding: utf-8 -*-
 
 # ------------- Búsquedas en cadenas -------------
 
+
+# In[2]:
+
+
 # Cadena original
 cadena = "Amor y deseo son dos cosas diferentes; que no todo lo que se ama se desea, ni todo lo que se desea se ama. (Don Quijote)"
+
+
+# In[3]:
+
+
 
 # Busca la cadena "ama"
 print (cadena.find("ama"))
 
+
+# In[6]:
+
+
 #Imprimie  la subcadena
 print (cadena[cadena.find("ama"):])
+
+
+# In[7]:
+
 
 # Busca la siguiente coincidencia de "ama"
 print (cadena[cadena.find("ama") + 1:].find("ama"))
 
+
+# In[10]:
+
+
 # Imprime la cadena a partir de la segunda coincidencia
 print (cadena[61 + 1 + 40:])
+
+
+# In[11]:
+
 
 # Busca "corazon" en la cadena
 print (cadena.find("corazon"))
 
+
+# In[12]:
+
+
 # Busca a partir de un indice
 print (cadena.find("todo", 62))
 
-# Imprime a partir de un �ndice y hasta el final
+# Imprime a partir de un �ndi
+
+
+# In[13]:
+
+
 print (cadena[78:])
+
+
+# In[ ]:
+
+
+
+
